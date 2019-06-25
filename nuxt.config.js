20758d51f5d15cce03877370c3f68c5c7af03119
yue.lab <gitlab@rainy.me>
2019-06-26 05:43:13 +0900

    update icon; add theme-color

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,7 @@ module.exports = {
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { name: ' theme-color', content: '#f08500' },
       {
         hid: 'description',
         name: 'description',
@@ -42,6 +43,6 @@ module.exports = {
     /*
      ** You can extend webpack config here
      */
-    extend(config, ctx) {}
+    extend(config, ctx) { }
   }
 }
