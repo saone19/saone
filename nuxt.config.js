efc027339133adb990d6435df04c772fe0671731
rainy-me <github@rainy.me>
2019-07-27 03:41:05 +0900

    add some seo support

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,8 +10,28 @@ module.exports = {
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
       { name: ' theme-color', content: '#f08500' },
       {
-        hid: 'description',
+        name: 'keywords',
+        content: 'digital hollywood university,dhu,study in japan,music video,shot film,web design,japan'
+      },
+      {
         name: 'description',
+        content: process.env.npm_package_description || ''
+      },
+      { name: 'og:title', content: 'SAONE Project' },
+      { name: 'og:site_name', content: 'SAONE' },
+      { name: 'og:url', content: 'saone.digital' },
+      {
+        name: 'og:description',
+        content: process.env.npm_package_description || ''
+      },
+      { name: 'og:type', content: 'website' },
+      { name: 'og:image', content: 'https://saone.digital/ogp.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:title', content: 'SAONE Project' },
+      { name: 'twitter:domain', content: 'saone.digital' },
+      { name: 'twitter:creator', content: '@nerd_yue' },
+      {
+        name: 'twitter:description',
         content: process.env.npm_package_description || ''
       }
     ],
